1a2ae1b7c714524614893e4aec7a0e716f1c9fec
Abdelrahman Ayman <95408654+Abdelrahman968@users.noreply.github.com>
2024-10-09 19:49:53 +0300

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+const { registerUser, loginUser } = require("../controllers/authController");
+
+router.post("/login", loginUser);
+router.post("/register", registerUser);
+
+module.exports = router;
