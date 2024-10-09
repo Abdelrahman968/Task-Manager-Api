2bc44248dff344975142890f8465c751744e3ba0
Abdelrahman Ayman <95408654+Abdelrahman968@users.noreply.github.com>
2024-10-09 19:50:45 +0300

    Add files via upload

---
+++ models/Task.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  status: {
+    type: String,
+    enum: ["pending", "in-progress", "completed"],
+    default: "pending",
+  },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const TaskData = mongoose.model("Task", taskSchema);
+
+module.exports = TaskData;
---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["admin", "user"], default: "user" },
+});
+
+const UserData = mongoose.model("User", userSchema);
+
+module.exports = UserData;
